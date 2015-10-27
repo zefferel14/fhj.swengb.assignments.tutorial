cd04bcfb5df8c5a29ace3dd5a561c5bafbd7562a
Robert Ladstätter <rladstaetter@gmail.com>
2015-10-27 19:11:55 +0100

    work on reporting

--- src/test/scala/fhj/swengb/assignments/tutorial/rladstaetter/TutorialAssignmentTest.scala
+++ src/test/scala/fhj/swengb/assignments/tutorial/rladstaetter/TutorialAssignmentTest.scala
@@ -6,17 +6,26 @@ import org.junit.Test
 class TutorialAssignmentTest {
 
   /**
-   * This test shows that your implementation is correct.
-   *
-   * To pass the first assignment:
-   *
-   * -> you have to implement a Scala Object named 'HelloWorldAssignment'
-   * -> you have to provide a method called "hello"
-   * -> you have to return a string with a specific content
-   * -> you have to put it up on your github account
-   * -> you have to create a pull request containing your github username
-   *
-   */
+    * This test shows that your implementation is correct.
+    *
+    * To pass the tutorial assignment:
+    *
+    * -> clone the tutorial github repo at
+    *    https://github.com/rladstaetter/fhj.swengb.assignments.tutorial
+    *
+    * -> you have to implement a Scala Object named 'TutorialAssignment'
+    *    in the package fh.swengb.assignments.tutorial.<yourId>
+    * -> you have to provide a method called "hello"
+    * -> you have to return a string with a specific content (see test)
+    * -> put the TutorialAssignmentTest in the same package like the
+    *    TutorialAssignment object.
+    *
+    * -> push it to your github account
+    *
+    * -> the project should have an address like this:
+    *    https://github.com/<yourid>/fhj.swengb.assignments.tutorial
+    *
+    */
   @Test def test(): Unit = {
     assertEquals("Hello world!", TutorialAssignment.hello())
   }
