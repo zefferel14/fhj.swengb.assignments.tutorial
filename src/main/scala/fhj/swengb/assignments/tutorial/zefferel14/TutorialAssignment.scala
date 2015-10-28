7cd18f546a5d17e89b74b2f134698363c4b94e92
zefferel14 <lukzefferer@hotmail.de>
2015-10-28 11:24:39 +0100

    done

--- src/main/scala/fhj/swengb/assignments/tutorial/zefferel14/TutorialAssignment.scala
+++ src/main/scala/fhj/swengb/assignments/tutorial/zefferel14/TutorialAssignment.scala
@@ -1,4 +1,4 @@
-package fhj.swengb.assignments.tutorial.rladstaetter
+package fhj.swengb.assignments.tutorial.zefferel14
 
 object TutorialAssignment {
   def hello() = "Hello world!"
--- src/test/scala/fhj/swengb/assignments/tutorial/zefferel14/TutorialAssignmentTest.scala
+++ src/test/scala/fhj/swengb/assignments/tutorial/zefferel14/TutorialAssignmentTest.scala
@@ -1,4 +1,4 @@
-package fhj.swengb.assignments.tutorial.rladstaetter
+package fhj.swengb.assignments.tutorial.zefferel14
 
 import org.junit.Assert._
 import org.junit.Test
